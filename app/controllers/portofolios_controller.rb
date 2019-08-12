1331db648132476e68b3d8b18bb1a9f1adc2f8bd
Francisca <frastengo@gmail.com>
2019-08-12 12:48:51 -0700

    integrated new and create for portofolio

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -2,4 +2,21 @@ class PortofoliosController < ApplicationController
   def index
     @portofolio_items = Portofolio.all
   end
+
+  def new
+    @portofolio_item = Portofolio.new
+  end
+
+  def create
+    @portofolio_item = Portofolio.new(params.require(:portofolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portofolio_item.save
+        format.html{redirect_to portofolios_path, notice: "Your portofolio item is now live."}
+      else
+        format.html { render :new}
+      end
+    end
+  end
+
 end
