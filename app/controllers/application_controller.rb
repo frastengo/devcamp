15eb113c3cf62a637ddbdfe4da3fc245c6ae98ed
Francisca <frastengo@gmail.com>
2019-08-15 14:47:53 -0700

    virtual attr user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   ## run this before you run anything else in the controller
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseWhitelist
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+
 
 end
