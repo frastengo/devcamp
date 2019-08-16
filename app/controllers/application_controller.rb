9b8990595dd797915efeea88a820e947b1dee5c1
Francisca <frastengo@gmail.com>
2019-08-15 17:35:53 -0700

    implemented query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   ## run this before you run anything else in the controller
   include DeviseWhitelist
 
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
+
 
 
 end
