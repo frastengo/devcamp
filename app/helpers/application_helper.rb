4f1b15b0091a637db36be91951b30381aa858c45
Francisca <frastengo@gmail.com>
2019-08-19 11:20:00 -0700

    implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  ##regular ruby methods, the purpose is to whenever you need some conditional logic
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Login", new_user_session_path) + "<br>".html_safe +
+      (link_to "Register", new_user_registration_path)
+    end
+  end
+end
+
+def sample_method
+  x = 1
+  if x ==1
+    "yay"
+  else
+    "no"
+  end
 end
