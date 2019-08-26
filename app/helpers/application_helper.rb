55ce5d493b89e2b640835e3e1165713519e1bb50
Francisca <frastengo@gmail.com>
2019-08-26 15:28:43 -0700

    added alerts and an alert generator on app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,6 +76,23 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts 
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert 
+      alert_generator alert
+    end
+
+  end 
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Alert:", sticky: false, time: 2000)
+  end
+
+
+
+
+
 
 end
 
