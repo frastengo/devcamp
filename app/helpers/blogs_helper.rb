c0b2f8c5114310f0c365a17b5bb0fd782c27bcfb
Francisca <frastengo@gmail.com>
2019-08-27 14:09:43 -0700

    final chat commit

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,5 @@
 module BlogsHelper
+    def gravatar_helper  
+        image_tag "https://vetstreet-brightspot.s3.amazonaws.com/da/5a/a038b1d94ede931d0fa8f8618f77/socialized-puppy-thinkstock-158913287-335sm12313.jpg", width: 40
+    end 
 end
